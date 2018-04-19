b22461a42a20b81ef3b7d9141417b4b3c45b93cc
Jay Billings <jbillings@visitseattle.org>
2018-04-19 13:45:03 -0700

    Connects events layout to data
    * Using socketio

--- src/layouts/EventsLayout.jsx
+++ src/layouts/EventsLayout.jsx
@@ -1,12 +1,34 @@
 import React, {Component} from 'react';
-import Header from "../components/common/Header";
+import app from '../services/socketio';
+
+import Header from '../components/common/Header';
 
 export default class EventsLayout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const eventsService = app.service('events');
+
+    eventsService.find({
+      query: {
+        $sort: {created_at: -1},
+        $limit: 25
+      }
+    }).then((eventsData) => {
+      console.log(eventsData.data);
+      this.setState({'eventsData': eventsData});
+    });
+  }
+
   render() {
     return (
       <div className="container">
         <Header/>
         <h2>Events</h2>
+        <p>{JSON.stringify(this.state.eventsData)}</p>
         <h3>View/Modify</h3>
         <h3>Add New Event</h3>
       </div>
