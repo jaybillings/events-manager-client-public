00412b5fbe4a2461e44ad055d8c551505e20f190
Jay Billings <jbillings@visitseattle.org>
2018-04-24 13:29:16 -0700

    `SingleEventLayout` now handling data

--- src/components/events/EventRecord.jsx
+++ src/components/events/EventRecord.jsx
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-import {Redirect} from 'react-router';
-import app from '../../services/socketio';
 
 import '../../styles/listing-forms.css';
 
@@ -8,53 +6,15 @@ export default class EventRecord extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {isLoading: true, hasChanged: false};
-    this.eventsService = app.service('events');
-
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.deleteEvent = this.deleteEvent.bind(this);
-  }
-
-  componentDidMount() {
-    this.eventsService.get(this.props.eventId).then((message) => this.setState({'event': message, isLoading: false}));
   }
 
   handleChange() {
     this.refs.submitButton.disabled = false;
   }
 
-  handleSubmit(e) {
-    const newData = {
-      name: this.refs.nameInput.value.trim(),
-      start_date: this.refs.startInput.value,
-      end_date: this.refs.endInput.value,
-      description: this.refs.descInput.value.trim()
-    };
-
-    e.preventDefault();
-
-    this.eventsService.patch(this.props.eventId, newData).then((message) => {
-      this.setState({event: message});
-      this.refs.submitButton.disabled = true;
-    });
-  }
-
-  deleteEvent() {
-    // TODO: Only administrators should be able to delete
-    this.eventsService.remove(this.props.eventId).then(this.setState({hasDeleted: true}));
-  }
-
   render() {
-    if (this.state.isLoading) {
-      return <p>Data loading... Please wait...</p>;
-    }
-
-    if (this.state.hasDeleted) {
-      return <Redirect to={'/events'}/>
-    }
-
-    const listing = this.state.event;
+    const listing = this.props.event;
     const dateFormatOptions = {
       year: "numeric", month: "numeric", day: "numeric",
       hour: "numeric", minute: "numeric", second: "numeric"
@@ -64,10 +24,10 @@ export default class EventRecord extends Component {
     const createdReadable = listing.created_at ? new Date(listing.created_at).toLocaleString('en-US', dateFormatOptions) : '';
 
     return (
-      <form id="event-listing-form" onSubmit={this.handleSubmit} onChange={this.handleChange}>
+      <form id="event-listing-form" onSubmit={this.props.handleSubmit} onChange={this.handleChange}>
         <div>
           <button type="submit" ref="submitButton" disabled>Save Changes</button>
-          <button type="button" ref="deleteButton" onClick={this.deleteEvent}>Delete Event</button>
+          <button type="button" ref="deleteButton" onClick={this.props.deleteEvent}>Delete Event</button>
         </div>
         <label>
           ID
--- src/layouts/SingleEventLayout.jsx
+++ src/layouts/SingleEventLayout.jsx
@@ -1,15 +1,75 @@
 import React, {Component} from 'react';
+import {Redirect} from 'react-router';
+import app from '../services/socketio';
 
 import Header from '../components/common/Header';
 import EventRecord from '../components/events/EventRecord';
 
 export default class SingleEventLayout extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {event: {}, isLoaded: false, hasDeleted: false, notFound: false};
+    this.eventsService = app.service('events');
+
+    this.deleteEvent = this.deleteEvent.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderRecord = this.renderRecord.bind(this);
+  }
+
+  componentDidMount() {
+    const id = this.props.match.params.id;
+
+    this.eventsService.get(id).then(message => {
+      this.setState({event: message, isLoaded: true});
+    }, (message) => {
+      console.log('error', message);
+      this.setState({notFound: true});
+    });
+  }
+
+  deleteEvent() {
+    const id = this.state.event.id;
+
+    // TODO: Only administrators should be able to delete
+    this.eventsService.remove(id).then(this.setState({hasDeleted: true}));
+  }
+
+  handleSubmit(e) {
+    const id = this.state.event.id;
+    const newData = {
+      name: this.refs.record.refs.nameInput.value.trim(),
+      start_date: this.refs.record.refs.startInput.value,
+      end_date: this.refs.record.refs.endInput.value,
+      description: this.refs.record.refs.descInput.value.trim()
+    };
+
+    e.preventDefault();
+
+    this.eventsService.patch(id, newData).then((message) => {
+      console.log('patch', message);
+      this.setState({event: message});
+    }, (message) => {
+      console.log('error', message);
+    });
+  }
+
+  renderRecord() {
+    if (!this.state.isLoaded) return <p>Data is loading... Please be patient...</p>;
+
+    return <EventRecord ref="record" event={this.state.event} handleSubmit={this.handleSubmit} deleteEvent={this.deleteEvent} />;
+  }
+
   render() {
+    if (this.state.notFound) return <Redirect to={'/404'} />;
+
+    if (this.state.hasDeleted) return <Redirect to={'/events'} />;
+
     return (
       <div className={'container'}>
         <Header/>
-        <h2>Event Name</h2>
-        <EventRecord eventId={this.props.match.params.id} />
+        <h2>{this.state.event.name}</h2>
+        {this.renderRecord()}
       </div>
     );
   }
