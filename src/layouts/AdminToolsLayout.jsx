90e2e8d2ad70bc94ee6513256a3e6f7f59671f8b
Jay Billings <jbillings@visitseattle.org>
2018-09-07 16:27:31 -0700

    Initial scaffolding for admin export function

--- src/layouts/AdminToolsLayout.jsx
+++ src/layouts/AdminToolsLayout.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
 import Header from '../components/common/Header';
+import AdminExport from './admin/AdminExport';
 
 export default class AdminToolsLayout extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class AdminToolsLayout extends Component {
         <h2>Admin Tools</h2>
         <h3>Manage Users</h3>
         <h3>Import/Export</h3>
+        <AdminExport />
         <h3>Create API Buckets</h3>
       </div>
     );
