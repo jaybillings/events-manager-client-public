b77470c8c6c56e86934198d78eee966c94ab7b39
Jay Billings <jbillings@visitseattle.org>
2018-09-13 13:12:51 -0700

    Adds link to /exporter endpoint on admin page

--- src/layouts/AdminToolsLayout.jsx
+++ src/layouts/AdminToolsLayout.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 
 import Header from '../components/common/Header';
-import AdminExport from './admin/AdminExport';
 
 export default class AdminToolsLayout extends Component {
   render() {
@@ -11,7 +10,7 @@ export default class AdminToolsLayout extends Component {
         <h2>Admin Tools</h2>
         <h3>Manage Users</h3>
         <h3>Import/Export</h3>
-        <AdminExport />
+        <a className="button" href={'http://localhost:3030/exporter/json'} target={'_blank'}>Export All Data</a>
         <h3>Create API Buckets</h3>
       </div>
     );
