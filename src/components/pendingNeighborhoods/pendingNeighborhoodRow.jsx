74839cbcf340b117d277456f7ccbcb169f9098dc
Jay Billings <jbillings@visitseattle.org>
2018-10-09 16:21:25 -0700

    Adds empty files for new pending schema views

---
+++ src/components/pendingNeighborhoods/pendingNeighborhoodRow.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class pendingNeighborhoodRow extends Component {
+  render() {
+    return (
+
+    );
+  }
+};
---
+++ src/components/pendingNeighborhoods/pendingNeighborhoodsModule.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class pendingNeighborhoodsModule extends Component {
+  render() {
+    return (
+
+    );
+  }
+};
