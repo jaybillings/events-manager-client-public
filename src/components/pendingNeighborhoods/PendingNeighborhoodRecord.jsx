8d41625c556c60b7cadc2f0285c623062e8e222c
Jay Billings <jbillings@visitseattle.org>
2018-10-11 15:56:04 -0700

    Adds single pending neighborhood views

---
+++ src/components/pendingNeighborhoods/PendingNeighborhoodRecord.jsx
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import Moment from 'moment';
+
+import '../../styles/schema-record.css';
+import '../../styles/toggle.css';
+
+export default class PendingNeighborhoodRecord extends Component {
+  constructor(props) {
+    super(props);
+
+    this.nameInput = React.createRef();
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClickDelete = this.handleClickDelete.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const id = this.props.pendingNeighborhood.id;
+    const newData = {name: this.nameInput.current.value.trim()};
+
+    this.props.saveNeighborhood(id, newData);
+  }
+
+  handleClickDelete() {
+    const id = this.props.pendingNeighborhood.id;
+    this.props.deleteNeighborhood(id);
+  }
+
+  render() {
+    const pendingHood = this.props.pendingNeighborhood;
+    const hoodId = this.props.pendingNeighborhood['targetId'] || 'N/A';
+    const createdAt = Moment(pendingHood['created_at']).calendar();
+    const updatedAt = Moment(pendingHood['updated_at']).calendar();
+
+    return (
+      <form id={'pending-hood-listing-form'} className={'schema-record'} onSubmit={this.handleSubmit}>
+        <label>
+          Live Neighborhood ID
+          <input type={'text'} defaultValue={hoodId} disabled />
+        </label>
+        <label>
+          Created
+          <input type={'text'} defaultValue={createdAt} disabled />
+        </label>
+        <label>
+          Last Updated
+          <input type={'text'} defaultValue={updatedAt} disabled />
+        </label>
+        <label className={'required'}>
+          Name
+          <input type={'text'} ref={this.nameInput} defaultValue={pendingHood.name} required maxLength={100} />
+        </label>
+        <div className={'block-warning'} title={'Caution: This event is pending. It must be pushed live before it is visible on the site.'}>
+          <button type="submit" className="button-primary">Save Changes</button>
+          <button type="button" onClick={this.handleClickDelete}>Discard Neighborhood</button>
+        </div>
+      </form>
+    );
+  }
+};
---
+++ src/layouts/single/SinglePendingNeighborhoodLayout.jsx
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+import {Redirect} from 'react-router';
+import app from '../../services/socketio';
+
+import Header from '../../components/common/Header';
+import PendingNeighborhoodRecord from '../../components/pendingNeighborhoods/PendingNeighborhoodRecord';
+
+export default class SinglePendingNeighborhoodLayout extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {pendingHood: {}, hoodLoaded: false, hasDeleted: false, notFound: false};
+
+    this.pendingHoodsService = app.service('pending-neighborhoods');
+
+    this.fetchAllData = this.fetchAllData.bind(this);
+    this.renderRecord = this.renderRecord.bind(this);
+    this.deleteNeighborhood = this.deleteNeighborhood.bind(this);
+    this.saveNeighborhood = this.saveNeighborhood.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchAllData();
+
+    this.setState({hoodLoaded: false});
+
+    // Register listeners
+    this.pendingHoodsService
+      .on('patched', message => {
+        this.setState({pendingHood: message, hoodLoaded: true});
+      })
+      .on('removed', () => {
+        this.setState({hasDeleted: true});
+      });
+  }
+
+  componentWillUnmount() {
+    this.pendingHoodService
+      .removeListener('patched')
+      .removeListener('removed');
+  }
+
+  fetchAllData() {
+    const id = this.props.match.params.id;
+
+    this.pendingHoodsService.get(id).then(message => {
+      this.setState({pendingHood: message, hoodLoaded: true});
+    }, message => {
+      console.log('error', JSON.stringify(message));
+      this.setState({notFound: true});
+    });
+  }
+
+  deleteNeighborhood(id) {
+    this.pendingHoodsService.remove(id).then(this.setState({hasDeleted: true}));
+  }
+
+  saveNeighborhood(id, newData) {
+    this.pendingHoodsService.patch(id, newData).then(message => {
+      console.log('patch', message);
+    }, err => {
+      console.log('error', err);
+    });
+  }
+
+  renderRecord() {
+    if (!this.state.hoodLoaded) return <p>Data is loading... Please be patient...</p>;
+
+    return <PendingNeighborhoodRecord pendingNeighborhood={this.state.pendingHood}
+                                      saveNeighborhood={this.saveNeighborhood}
+                                      deleteNeighborhood={this.deleteNeighborhood} />;
+  }
+
+  render() {
+    if (this.state.notFound) return <Redirect to={'/404'} />;
+
+    if (this.state.hasDeleted) return <Redirect to={`/import`} />;
+
+    return (
+      <div className={'container'}>
+        <Header />
+        <div className={'block-warning'}
+             title={'Caution: This event is pending. It must be pushed live before it is visible on the site.'}>
+          <h2>{this.state.pendingHood.name}</h2>
+        </div>
+        {this.renderRecord()}
+      </div>
+    );
+  }
+}
