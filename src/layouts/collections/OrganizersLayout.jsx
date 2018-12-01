7614c380145742595d68f4ad6041ea382ae782a3
Jay Billings <jbillings@visitseattle.org>
2018-11-30 16:46:38 -0800

    Adds child class for organizer add form

--- src/layouts/collections/OrganizersLayout.jsx
+++ src/layouts/collections/OrganizersLayout.jsx
@@ -1,7 +1,17 @@
+import React from "react";
 import ListingsLayout from "../../components/ListingsLayout";
+import OrganizerAddForm from "../../components/organizers/OrganizerAddForm";
 
 export default class OrganizersLayout extends ListingsLayout {
   constructor(props) {
     super(props, 'organizers');
+  }
+
+  renderAddForm() {
+    if (!this.state.listingsLoaded) return <p>Data is loading... Please be patient...</p>;
+
+    const listings = this.state.listings;
+
+    return <OrganizerAddForm listings={listings} createListing={this.createListing} />;
   }
 };
